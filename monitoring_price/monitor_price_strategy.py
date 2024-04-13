5d72a78e118725fce71b3171dd8d1a50e50f454a
apeironrod <101962768+apeironrod@users.noreply.github.com>
2024-04-13 08:58:36 +0700

    Update monitor_price_strategy.py

--- monitoring_price/monitor_price_strategy.py
+++ monitoring_price/monitor_price_strategy.py
@@ -1,7 +1,7 @@
 
 import time, os,sys
 from utils.computePrice import getSymbol, get_investment_worth, getQuoteToken
-from utils.alreadyBought import getSettings
+from utils.alreadyBought import getSettings 
 from configparser import ConfigParser  
  
 """
